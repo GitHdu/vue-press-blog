6e03497d068414effafb3fd98d2554093201e0bf
yangc <yangc@getui.com>
2018-09-10 19:39:49 +0800

    feat(components): add component example

---
+++ docs/.vuepress/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{a}}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        a: 12
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
